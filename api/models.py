7120e84784de4c9b3f378bae660f98556ed6da83
Umair Syed <itsumairsyed@gmail.com>
2023-03-22 23:42:26 +0500

    Category, product and productimage migration added

--- api/models.py
+++ api/models.py
@@ -3,17 +3,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     description = models.TextField()
     popular = models.BooleanField()
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now_add=True)
 
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
-    price_currency = models.enums('$')
+    price_currency = models.CharField(max_length=255)
     discount_percentage = models.FloatField()
     discounted_price = models.FloatField()
     quantity = models.IntegerField()
@@ -23,6 +23,14 @@ class Product(models.Model):
             MinValueValidator(1.0)
         ]
     )
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     popular = models.BooleanField()
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now_add=True)
+
+class ProductImage(models.Model):
+    image_url = models.TextField()
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
--- dummy_estore_api/settings.py
+++ dummy_estore_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'dummy_estore_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dummy_estore',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
