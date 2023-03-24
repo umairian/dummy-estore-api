8dc916fed4dd1c872773859f7a67982362bad7d2
Umair Syed <itsumairsyed@gmail.com>
2023-03-24 19:59:13 +0500

    Create category and get categories completed

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('/post', views.addCategory),
+    path('/categories', views.getCategories)
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import CategorySerializer
+from .models import Category
 
 # Create your views here.
 @api_view(['GET'])
@@ -8,3 +10,16 @@ def index(request):
     print(request)
     return Response("All Praise to ALLAH, the only Perfect!")
 
+@api_view(['POST'])
+def addCategory(request):
+    serializer = CategorySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response("Category added successfully")
+
+
+@api_view(['GET'])
+def getCategories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
